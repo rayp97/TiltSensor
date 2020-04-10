d6c393815ae76a528c6680d1b338294732e515f3
jongha.park <raybirdy@gmail.com>
2020-04-10 12:17:44 +0900

    custom view - 수평계 생성

--- app/src/main/java/com/example/tiltsensor/TiltView.kt
+++ app/src/main/java/com/example/tiltsensor/TiltView.kt
@@ -2,11 +2,38 @@ package com.example.tiltsensor
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.view.View
 
 class TiltView(context: Context?) : View(context) {
 
+    private val greenPaint: Paint = Paint()
+
+    private val blackPaint: Paint = Paint()
+
+    private var cX: Float = 0f
+    private var cY: Float = 0f
+
+    init {
+        //녹색 페인트
+        greenPaint.color = Color.GREEN
+
+        //검은색 페인트
+        blackPaint.style = Paint.Style.STROKE
+    }
+
     override fun onDraw(canvas: Canvas?) {
+        canvas?.drawCircle(cX, cY, 100f, blackPaint)
+        canvas?.drawCircle(cX, cY, 100f, greenPaint)
+
+        canvas?.drawLine(cX - 20, cY, cX + 20, cY, blackPaint)
+        canvas?.drawLine(cX, cY - 20, cX, cY + 20, blackPaint)
         
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        cX = w / 2f
+        cY = h / 2f
     }
 }
