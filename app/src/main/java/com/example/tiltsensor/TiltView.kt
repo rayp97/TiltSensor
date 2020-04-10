51752babe82e5cd0b9364abd6a54909da12e699f
jongha.park <raybirdy@gmail.com>
2020-04-10 14:17:55 +0900

    custom view - 수평계 로직 추가

--- app/src/main/java/com/example/tiltsensor/TiltView.kt
+++ app/src/main/java/com/example/tiltsensor/TiltView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.hardware.SensorEvent
 import android.view.View
 
 class TiltView(context: Context?) : View(context) {
@@ -14,6 +15,9 @@ class TiltView(context: Context?) : View(context) {
 
     private var cX: Float = 0f
     private var cY: Float = 0f
+
+    private var xCoord: Float = 0f
+    private var yCoord: Float = 0f
 
     init {
         //녹색 페인트
@@ -35,5 +39,12 @@ class TiltView(context: Context?) : View(context) {
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         cX = w / 2f
         cY = h / 2f
+    }
+
+    fun onSensorEvent(event: SensorEvent) {
+        yCoord = event.values[0] * 20
+        xCoord = event.values[1] * 20
+
+        invalidate()
     }
 }
