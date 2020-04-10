73f251237bd28d3a134a25153b2d8d8ac11a7c70
jongha.park <raybirdy@gmail.com>
2020-04-10 11:43:44 +0900

    Sensor 사용 준비

--- app/src/main/java/com/example/tiltsensor/MainActivity.kt
+++ app/src/main/java/com/example/tiltsensor/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.tiltsensor
 
+import android.content.Context
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val sensorManager by lazy { // 초기화 지연
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
